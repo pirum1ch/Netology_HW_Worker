eb7e0f42d1c4120910320782b8a4ff004f58cf11
Pirumov Dmitry <dmitry.pirumov@gmail.com>
2023-07-31 18:04:43 +0300

    initional commit

---
+++ src/Worker.java
@@ -0,0 +1,24 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+    public Worker(OnTaskDoneListener callback){
+        this.callback = callback;
+
+    }
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback){
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if(i==33){
+                errorCallback.onError("\n !!!!!! -- Task " + i + " is in a error --- !!!!!!! \n");
+            }else{
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+}
